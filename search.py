20466c807eb3538aa89091870a98ea5dc7321fd4
JP <110874723+voracityyy@users.noreply.github.com>
2024-09-24 17:50:54 -0700

    Add files via upload

--- search.py
+++ search.py
@@ -14,30 +14,29 @@ def handle_json_requests():
     option = data.get('option')
     # idea - buttons that are properly labelled should choose option, 
     # search term prompt appears for each option except search_all 
-    search_term = data.get('search_term') # valid search terms are: 'all', 'title', 'id', 'publisher', 'year'
-    select_search(option, search_term) # if search_all is selected this, value of search_term should not matter
+    search_term = data.get('search_term') 
+    # valid search terms are: 'all', 'title', 'id', publisher', 'year', 'availability'
+    jsonify(select_search(option, search_term)) 
+    # if search_all is selected this, value of search_term should not matter & should not be prompted
+    # if check_availability is selected, search_term must be id
 
-def select_search(option, search_term): # note: i need to jsonify
+def select_search(option, search_term):
     match option:
             case 'all':
-                return search_all()
-            case 'available':
-                return search_available(search_term)
+                search_all()
             case 'title':
-                return search_title(search_term)
+                search_title(search_term)
             case 'id':
-                return search_id(search_term)
+                search_id(search_term)
             case 'publisher':
-                return search_publisher(search_term)
+                search_publisher(search_term)
             case 'year':
-                return search_year(search_term)
+                search_year(search_term)
+            case 'availability':
+                check_availability(search_term)
 
 def search_all():
     return pd.read_csv('VideoGames.csv')
-
-def search_available():
-    return('test')
-# WIP - i need to compare entries in rentals table for this to work
 
 def search_title(title):
     df = pd.read_csv('VideoGames.csv')
@@ -55,3 +54,13 @@ def search_year(year):
     df = pd.read_csv('VideoGames.csv')
     return df[df['Year'].str.contains(year, case=False, na=False)]
 # WIP - year range should be a better option 
+def check_availability(id):
+    video_games = pd.read_csv('VideoGames.csv')
+    rentals = pd.read_csv('Rentals.csv')
+    cnt = len(rentals[(rentals['VideoGameID']==id) & rentals['ReturnDate'].isna()])
+    
+    # df1 = pd.DataFrame(data={'Status': ['Available'], 'OutForRent': [cnt], 'MaxStock': []})
+    # return if (cnt < video_games.at[1, 'MaxStock']) else return pd.DataFrame
+    print(cnt)
+# WIP
+check_availability('V0001')

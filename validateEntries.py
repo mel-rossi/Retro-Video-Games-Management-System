2d423563592f00ca5c65c226222673d16148c0bc
mel-rossi <melani.r.rodrigues@gmail.com>
2024-09-25 19:56:54 -0700

    ValidateVideoGameID & ValidateMemberID Added

--- validateEntries.py
+++ validateEntries.py
@@ -1,14 +1,42 @@
 # Validation & Valid Generation methods 
-import pandas as pd1
+# Currently a Work In Progress
+import pandas as pd
+
+# Data Frames 
+
+# Read Rentals 
+df1 = pd.read_csv('Inventory/Rentals.csv')
+
+# Read VideoGames
+df2 = pd.read_csv('Inventory/VideoGames.csv')
+
+# Read Members
+df3 = pd.read_csv('Inventory/Members.csv')
  
 def generateRentalID():
-    # Read CSV into DataFrame
-    df = pd1.read_csv("Inventory/Rentals.csv")
-
-    for rentalID in df['RentalID']:
+    # Iterate through the column RentalID
+    for rentalID in df1['RentalID']: 
         pass
 
     return rentalID + 1
+# generateRentalID()
 
+def validateVideoGameID(VideoGameInput): 
+    # Iterate through the column VideoGameID
+    for VideoGameID in df2['VideoGameID']: 
+        if VideoGameID == VideoGameInput: 
+            return True 
+    
+    print("Invalid VideoGameID. Try Again!")
+    return False 
+# validateVideoGameID
 
-# print(generateRentalID())
+def validateMemberID(MemberInput): 
+    # Iterate through the column MemberID
+    for MemberID in df3['MemberID']: 
+        if MemberInput == MemberID: 
+            return True
+
+    print("Invalid MemberID. Try Again!")
+    return False
+# validateMemberID

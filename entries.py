e542984223a3b124126f2e6a2cda9acf50b00349
mel-rossi <melani.r.rodrigues@gmail.com>
2024-09-25 19:55:55 -0700

    appending bug fix in progress

--- entries.py
+++ entries.py
@@ -1,29 +1,57 @@
+# This file allows the user to add new entries to the Rentals Table 
+
+# Imports
 import pandas as pd
 from validateEntries import generateRentalID
+from validateEntries import validateVideoGameID
+from validateEntries import validateMemberID
 
-# Get user input for a value
-VideoGameID = input("Enter Video Game ID of Video Game Entered: ")
-MemberID = input("Enter Member ID: ")
+# Get user input for values
+
+# Call generateRentalID
+RentalID = generateRentalID()
+
+# Get VideoGameID
+validGameID = False
+while validGameID == False:
+    VideoGameID = input("Enter Valid Video Game ID (V####): ")
+    validGameID = validateVideoGameID(VideoGameID)
+    
+# Get MemberID
+validMemberID = False
+while validMemberID == False:
+    MemberID = input("Enter Valid Member ID (M####): ")
+    validMemberID = validateMemberID(MemberID)
+
 StartDate = input("Enter Start Date of Rental: ") 
-ReturnDate = input("Enter Renturn Date of Rental (leave blank if it hasn't been returned): ")
+ReturnDate = input("Enter Return Date of Rental (leave blank if it hasn't been returned): ")
 
 if ReturnDate == '':
     Status = 'Active'
 else:
     Status = 'Inactive'
 
-# Create a DataFrame 
-df = pd.DataFrame({'RentalID': [generateRentalID()], 
-        'VideoGameID': [VideoGameID], 
-        'MemberID': [MemberID], 
-        'StartDate': [StartDate],
-        'ReturnDate': [ReturnDate], 
-        'Status': [Status]})
+# Load CSV file
+df = pd.read_csv('Inventory/Rentals.csv')
 
-# Write DataFrame to a csv file 
-# by appending (no longer overwrites file)
-with open('Inventory/Rentals.csv', 'a', newline='') as f: 
-    df.to_csv(f, header=False, index=False)
+# Create a DataFrame
+
+# Create a new row (dictionary)
+row = {'RentalID': RentalID, 
+       'VideoGameID': VideoGameID,
+       'MemberID': MemberID, 
+       'StartDate': StartDate, 
+       'ReturnDate': ReturnDate, 
+       'Status': Status}
+
+# Convert dictionary to Data Frame
+df = pd.DataFrame.from_dict([row])
+
+# Append new row to CSV file
+with open('Inventory/Rentals.csv', 'a', newline='', encoding='utf8') as f: 
+  df.to_csv(f, header=False, index=False sep=',')
+
+# df.to_csv('Inventory/Rentals.csv', mode='a', header=False, index=False)
 
 # Display the content of DataFrame
 print(df.to_string())

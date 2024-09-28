f64eb34fe6f56cd9123629ce79e5c366728b64b6
mel-rossi <melani.r.rodrigues@gmail.com>
2024-09-28 12:13:14 -0700

    Splitting add and edit entries

---
+++ addEntries.py
@@ -0,0 +1,67 @@
+# This file allows the user to add new entries to the Rentals Table 
+
+# Imports
+import pandas as pd
+from validateEntries import generateRentalID
+from validateEntries import validateVideoGameID
+from validateEntries import confirmVideoGameID
+from validateEntries import validateMemberID
+from validateEntries import confirmMemberID
+from validateEntries import generateDate
+
+# Get user input and generate valid values
+
+# Call generateRentalID
+RentalID = generateRentalID()
+
+# Get VideoGameID
+validGameID = False
+confirmGameID = ""
+while confirmGameID != "y": # Exits loop when validGameID == True and confirmGameID == "y"
+    VideoGameID = input("Enter Valid Video Game ID (V####): ")
+    validGameID = validateVideoGameID(VideoGameID)
+    if validGameID == True:
+        confirmVideoGameID(VideoGameID)
+        confirmGameID = input("Enter 'y', if it is correct: ")
+           
+# Get MemberID
+validMemberID = False
+confirmMember = ""
+while confirmMember != "y": # Exits loop when validMemberID == True and confirmGameID == "y"
+    MemberID = input("Enter Valid Member ID (M####): ")
+    validMemberID = validateMemberID(MemberID)
+    if validMemberID == True:
+        confirmMemberID(MemberID)
+        confirmMember = input("Enter 'y', if it is correct: ")
+
+# For pratical purposes, generate today's date. As the log would generally be made alongside the Rental, therefore on the same date
+StartDate = generateDate()
+
+ReturnDate = input("Enter Return Date of Rental (leave blank if it hasn't been returned): ")
+
+if ReturnDate == '':
+    Status = 'Active'
+else:
+    Status = 'Inactive'
+
+# Read the CSV fiel into a DataFrame
+df = pd.read_csv("Inventory/Rentals.csv")
+
+# Add a new row using loc
+row = {'RentalID': RentalID, 
+       'VideoGameID': VideoGameID,
+       'MemberID': MemberID, 
+       'StartDate': StartDate, 
+       'ReturnDate': ReturnDate, 
+       'Status': Status}
+
+df.loc[len(df)] = row
+
+# Write the updated DataFrame back to the CSV file
+df.to_csv("Inventory/Rentals.csv", index=False)
+
+# Display the content of DataFrame
+print(df.to_string())
+
+today_date = pd.Timestamp.today().date()
+print(today_date)

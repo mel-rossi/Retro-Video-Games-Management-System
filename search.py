1947bfbb7318b3562cd6a3f1ea2b86eee0b87641
JP <110874723+voracityyy@users.noreply.github.com>
2024-10-03 12:28:49 -0700

    cleaned up output files

--- search.py
+++ search.py
@@ -3,35 +3,6 @@ from flask import Flask, request
 
 app = Flask(__name__)
 
-@app.route('/search', methods=['POST'])
-def search():
-    data = request.json
-    option = data['option']
-    # valid option values are: 'all', 'title', 'id', publisher', 'year', 'availability'
-    # idea - frontend javascript should have buttons, properly labelled, and when clicked
-    # sends a value to this python program
-    # input should be prompted after option is selected
-    match option:
-            case 'all':
-                result = search_all() 
-                # no input
-            case 'title':
-                result = search_title(data.get('input')) 
-                # input = title
-            case 'id':
-                result = search_id(data.get('input')) 
-                # input = videogameid
-            case 'publisher':
-                result = search_publisher(data.get('input')) 
-                # input = publisher
-            case 'year':
-                result = search_year(data.get('first_input'), data.get('second_input'))
-                # input = start year (first_input) & end year (second_input)
-            case 'available':
-                result = search_available()
-                # no input
-    result.to_html(open('searchResults.html', 'w'))
-
 def search_all():
     return pd.read_csv('VideoGames.csv')
 
@@ -53,7 +24,37 @@ def search_year(start_year, end_year):
  
 def search_available():
     df = pd.read_csv('VideoGames.csv')
-    return df[(df['Availability'] == 'Available')]
+    return df[(df['Availability'] == 'Available')].drop(columns='Availability')
+
+@app.route('/search', methods=['POST'])
+def output():
+    data = request.json
+    option = data['option']
+    # valid option values are: 'all', 'title', 'id', publisher', 'year', 'availability'
+    # idea - frontend javascript should have buttons, properly labelled, and when clicked
+    # sends a value to this python program
+    # input should be prompted after option is selected
+    match option:
+            case 'all':
+                results = search_all() 
+                # no input
+            case 'title':
+                results = search_title(data.get('input')) 
+                # input = title
+            case 'id':
+                results = search_id(data.get('input')) 
+                # input = videogameid
+            case 'publisher':
+                results = search_publisher(data.get('input')) 
+                # input = publisher
+            case 'year':
+                results = search_year(data.get('first_input'), data.get('second_input'))
+                # input = start year (first_input) & end year (second_input)
+            case 'available':
+                results = search_available()
+                # no input
+    with open('searchResults.html', 'w') as f:
+        results.to_html(f, index=False)
 
 if __name__ == '__main__':
     app.run(debug=True)

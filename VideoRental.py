7c7b057000b770678f3916b84e285334a3ac887a
mel-rossi <melani.r.rodrigues@gmail.com>
2024-10-05 12:33:50 -0700

    VideoRental.py Set Up

---
+++ VideoRental.py
@@ -0,0 +1,18 @@
+# This program Shows Hisory based off of the Video Game instead of the Member
+
+# BIG WIP
+
+# Imports 
+import pandas as pd 
+
+# Print out Rental Information of VideoGameID
+
+# Calculate average Rental Time (Start - Return Date) 
+
+# How many times VideoGame ID has been rented out 
+
+# Rank based on number of times VideoGames have been rented out 
+
+# How many VideoGame IDs are currently active rentals
+
+# How many rentals have there been ever 

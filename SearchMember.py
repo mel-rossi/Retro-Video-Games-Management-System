4156b87f123bad2b71457de4587364490344c707
Alex P <143479828+MariaAlexandraPeralejo@users.noreply.github.com>
2024-10-11 12:13:50 -0700

    Update SearchMember.py

--- SearchMember.py
+++ SearchMember.py
@@ -19,7 +19,7 @@ def member_layout(row):
     }
 
 # Search member based on input
-def search_member(user_input):
+def find_member(user_input):
     if user_input.isdigit() and len(user_input) == 4:
         user_input = "M" + user_input
     if user_input.startswith("M") and len(user_input) == 5:
@@ -29,19 +29,24 @@ def search_member(user_input):
         member = members_df[members_df['PhoneNumber'].str.replace("-","").str.replace(" ","") == updated_input]
 
     if not member.empty:
-        return member_layout(member.iloc[0])
-    return None
+        #return member_layout(member.iloc[0])
+        return member.iloc[0].astype(object).to_dict()
+    else:
+        #return jsonify({"error": "No member found with the provided ID or phone number."}), 404
+        return None
 
 #Flask endpoint to search member
 
 @app.route('/search_member', methods=['GET'])
-def search():
+def search_member_route():
     user_input = request.args.get('input')
-    member = search_member(user_input)
+    member = find_member(user_input)
+    
     if member:
         return jsonify(member), 200
+        #return jsonify(member.to_dict()), 200
     else:
         return jsonify ({"error": "No member found with the provided ID or phone number."}), 404
     
-    if __name__ == '__main__':
-        app.run(debug=True)
+if __name__ == '__main__':
+    app.run(debug=True)

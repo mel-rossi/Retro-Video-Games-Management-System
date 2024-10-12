a378c1b9378dff62c05ea827d87b1500f9235d07
alex45101 <aleontiev00@gmail.com>
2024-10-12 12:28:00 -0700

    Grabbing from search python file

--- front-end/js/availableGamesScript.js
+++ front-end/js/availableGamesScript.js
@@ -2,7 +2,7 @@ function searchGames(){
     let searchOption = "all";
     let searchInput = "";
 
-    $.ajax({        
+    /*$.ajax({        
         type: "POST",
         url: "../../search.py",
         data: JSON.stringify({
@@ -15,5 +15,19 @@ function searchGames(){
         error: function(err){
             console.error('Error in grabbing games:', err);
         }
-    })
+    })*/
+
+    var data = {'option': 'all'}; //change this for each button click or somrthing
+            var xhr = new XMLHttpRequest();
+            xhr.open('POST', 'http://localhost:5500/search', true);
+            xhr.setRequestHeader('Content-Type', 'application/json');
+            xhr.onload = function () {
+                if (xhr.status >= 200 && xhr.status < 300) {
+                    var response = JSON.parse(xhr.responseText); //change code to store it in a var or something idk
+                    console.log('Response:', response);
+                } else {
+                    console.error('Request failed with status:', xhr.status);
+                }
+            };
+            xhr.send(JSON.stringify(data));                     
 }

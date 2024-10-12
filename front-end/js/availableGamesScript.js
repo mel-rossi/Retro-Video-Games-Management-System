e0a2caa61db33c4e1c3e59e8e186d1cba4da54d1
alex45101 <aleontiev00@gmail.com>
2024-10-12 12:48:49 -0700

    playing with search

--- front-end/js/availableGamesScript.js
+++ front-end/js/availableGamesScript.js
@@ -1,23 +1,8 @@
 function searchGames(){
-    let searchOption = "all";
+    let searchOption = "publisher";
     let searchInput = "";
 
-    /*$.ajax({        
-        type: "POST",
-        url: "../../search.py",
-        data: JSON.stringify({
-            option: searchOption,
-            input: searchInput            
-        }),
-        success: function(response) {
-            console.log(response);
-        },
-        error: function(err){
-            console.error('Error in grabbing games:', err);
-        }
-    })*/
-
-    var data = {'option': 'all'}; //change this for each button click or somrthing
+    var data = {'option': searchOption, 'first_input': searchInput, 'second_input': ''}; //change this for each button click or somrthing
             var xhr = new XMLHttpRequest();
             xhr.open('POST', 'http://localhost:5500/search', true);
             xhr.setRequestHeader('Content-Type', 'application/json');

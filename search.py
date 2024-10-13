498ce270d1be4bfccf8c690961d6339f01780089
JP <110874723+voracityyy@users.noreply.github.com>
2024-10-12 19:04:55 -0700

    added search_general()

--- search.py
+++ search.py
@@ -33,6 +33,20 @@ def search_available():
     df = pd.read_csv(CSV_FILE)
     return df[(df['Availability'] == 'Available')].drop(columns='Availability')
 
+def search_general(title, publisher, start_year, end_year, status):
+    df = pd.read_csv(CSV_FILE)
+
+    if (start_year is None | end_year is None):
+        return df[df['Title'].str.contains(title, case=False, na=False)
+            & (df['Publisher'].str.contains(publisher, case=False, na=False))
+            & (df['Availability'].str.contains(status, case=True,na=False))]
+    else:
+        return df[df['Title'].str.contains(title, case=False, na=False)
+            & (df['Publisher'].str.contains(publisher, case=False, na=False))
+            & (df['Year'] >= start_year) & (df['Year'] <= end_year)
+            & (df['Availability'].str.contains(status, case=True,na=False))]
+
+
 @app.route('/search', methods=['POST'])
 def output():
     data = request.json
@@ -51,7 +65,10 @@ def output():
             results = search_year(data.get('first_input'), data.get('second_input'), data.get('status'))
         case 'available':
             results = search_available()
-    
+        case 'all_params':
+            results = search_general(data.get('title'), data.get('publisher'), data.get('start_year'),
+                                     data.get('end_year'), data.get('status'))
+            
     return jsonify(results.to_dict(orient='records'))
 
 if __name__ == '__main__':

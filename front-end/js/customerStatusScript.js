9d5e2ab44ea53c6644d4229251d9156d4a565276
alex45101 <aleontiev00@gmail.com>
2024-10-18 18:49:24 -0700

    Added Searching with Member Id

--- front-end/js/customerStatusScript.js
+++ front-end/js/customerStatusScript.js
@@ -1,6 +1,7 @@
 //regex for valid email & phone number
 const regexEmail = new RegExp(/(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/);
 const regexPhone = new RegExp(/(\+\d{1,3}\s?)?((\(\d{3}\)\s?)|(\d{3})(\s|-?))(\d{3}(\s|-?))(\d{4})(\s?(([E|e]xt[:|.|]?)|x|X)(\s?\d+))?/);
+const regexId = new RegExp(/^\d{4}$/);
 
 const SEARCH_INPUT_ELEMENT = document.getElementById("searchInput");
 const SEARCH_CONTAINER_ELEMENT = document.getElementById("searchContainer");
@@ -18,7 +19,8 @@ const RENTAL_INFO = { [RENTAL_TYPES.ACTIVE]: CURRENT_RENTALS_ELEMENT, [RENTAL_TY
 const INPUT_TYPES = Object.freeze({
     INVALID: -1,
     EMAIL: 0,
-    PHONE: 1
+    PHONE: 1,
+    ID: 2
 });
 
 window.onload = () => {
@@ -35,7 +37,7 @@ window.onload = () => {
 function errorCheckInput(input) {
     //emails are lower case
     input = input.toLowerCase();
-    const results = [regexEmail.exec(input), regexPhone.exec(input)];
+    const results = [regexEmail.exec(input), regexPhone.exec(input), regexId.exec(input)];
 
     let validIndex = -1;
     for (let i = 0; i < results.length; i++) {
@@ -64,6 +66,9 @@ function searchCustomer() {
             break;
         case INPUT_TYPES.PHONE:
             console.log("phone");
+            break;
+        case INPUT_TYPES.ID:
+            console.log("id");
             break;
     }
 

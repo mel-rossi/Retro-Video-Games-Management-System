8d0ed76a3641dd3e0195424c3f8b0c0ead4bb4a7
JP <110874723+voracityyy@users.noreply.github.com>
2024-10-18 18:37:42 -0700

    modified so it accomodates blueprints, also fixed porting issues

--- VGIMS.py
+++ VGIMS.py
@@ -1,14 +1,16 @@
 from flask import Flask, render_template, send_from_directory
 import webbrowser
-import subprocess
 import os
 import signal
+from SearchGame import searchgame_bp
 
 # this program serves as the main entry point for the webapp
 # when ran, opens main page and runs all the flask apps on startup
 # also makes the url pretty instead of '.html'
 
 app = Flask(__name__, template_folder='front-end/html')
+
+app.register_blueprint(searchgame_bp, url_prefix='')
 
 @app.route('/css/<path:filename>') # needed to handle multiple static folders 
 def serve_css(filename):
@@ -18,26 +20,19 @@ def serve_css(filename):
 def serve_js(filename):
     return send_from_directory('front-end/js', filename)
 
-def run_services(): # runs flask apps as subprocesses
-    subprocess.Popen(['python', 'SearchGame.py']) 
-    subprocess.Popen(['python', 'MemberRental.py'])
-    subprocess.Popen(['python', 'GameRental.py'])
-    # add more in the future
-
 @app.route('/shutdown', methods=['POST']) # should be called when javascript detects the tab/app has been closed
 def shutdown(): # shuts down flask apps
     os.kill(os.getpid(), signal.SIGTERM)
     return 'Shutting down app'
 
-@app.route('/') # opens main page on startup
+@app.route('/VGIMS') # opens main page on startup
 def main_page():
     return render_template('mainPage.html')
 
-@app.route('/games') # opens available games page
+@app.route('/VGIMS/games') # opens available games page
 def search_games_page():
     return render_template('availableGames.html')
 
 if __name__ == '__main__':
-    run_services()
-    webbrowser.open('http://127.0.0.1:5000/') # opens browser
-    app.run(port=5000) # runs on port 5000, other flask apps should run on different ports
+    webbrowser.open('http://127.0.0.1:5500/VGIMS') # opens browser
+    app.run(port=5500) # runs on port 5500

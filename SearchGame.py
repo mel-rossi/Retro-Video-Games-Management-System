5a13bf3cc26dcc43e1305db1ed52abd77d14eadb
JP <110874723+voracityyy@users.noreply.github.com>
2024-10-28 16:38:31 -0700

    implemented search by ranks

--- SearchGame.py
+++ SearchGame.py
@@ -27,6 +27,7 @@ def filter_games(title=None, publisher=None, start_year=None, end_year=None, sta
     if publisher is not None: 
         filters = filters & df['Publisher'].str.contains(publisher, case=False, na=False)
 
+    # Filters 'Availability'
     if status is not None: 
         filters = filters & df['Availability'].str.contains(status, case=True, na=False)
 
@@ -39,7 +40,8 @@ def filter_games(title=None, publisher=None, start_year=None, end_year=None, sta
 
     # Filters 'Genre'
     if genre is not None: # based on genre
-        # genre input could be an array of genre terms such as ['action', 'adventure', 'shooter', etc...]
+        # genre input NEEDS to an array of genre terms such as ['action', 'adventure', 'shooter', etc...]
+        # applies even if it is just one genre term
         genre = '|'.join(genre)
         filters = filters & df['Genre'].str.contains(genre, case=False, na=False, regex=True)
         # for games with multiple genres, entering one genre will return entries with that contain that genre
@@ -47,12 +49,23 @@ def filter_games(title=None, publisher=None, start_year=None, end_year=None, sta
     return df[filters]
 # filter_games
 
+def sort_by_rank(results):
+    results.sort_values(by='Rank', ascending=True, inplace=True) # sort initial df by rank
+    if len(df) != len(results): # prevents unnecessary calculations if we want all games sorted by rank
+        results['Rank'] = range(1, len(results) + 1) # overwrite existing rank column values
+    return results
+# sort_by_rank
+
 @searchgame_bp.route('/search_game', methods=['POST'])
 def search_game_route():
 
     data = request.json # Get json data from POST body
-    results = filter_games(data.get('title'), data.get('publisher'), data.get('start_year'),
-                                     data.get('end_year'), data.get('status'), data.get('genre'))
+    results = filter_games(data.get('title'), data.get('publisher'), data.get('start_year'), data.get('end_year'),
+                           data.get('status'), data.get('genre'))
+    
+    # if parameter 'ranked' is not none (it can be anything else), then sort result by rank
+    if (data.get('ranked') is not None): 
+        results = sort_by_rank(results)
 
     return jsonify(results.to_dict(orient='records'))
 # search_game_route

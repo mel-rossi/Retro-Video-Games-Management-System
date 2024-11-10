caaaacfceb72d96dd6cae1f892ba5f55474a02a9
mel-rossi <melani.r.rodrigues@gmail.com>
2024-11-09 16:47:41 -0800

    Dry Run Request completed

--- EditMember.py
+++ EditMember.py
@@ -1,6 +1,7 @@
 import os 
 import pandas as pd 
 from flask_cors impport CORS
+from validateEntries import validateMemberID 
 from flask import request, jsonify, Blueprint, session 
 
 # This file allows the use to edit Member Registrations 
@@ -13,4 +14,17 @@ BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 INVENTORY_DIR = os.path.join(BASE_DIR, 'Inventory') 
 CSV_FILE = os.path.join(INVENTORY_DIR, 'Members.csv') 
 
-# Dry Run : Initial Input Validation 
+# Dry Run : Initial Input Validation
+def dry_run_request_member(MemberID): 
+
+    # Validate MemberID 
+    if not validateMemberID(MemberID): 
+        return jsonify({"error": "Invalid Rental ID"}), 400
+
+    session['MemberID'] = MemberID
+
+    return jsonify({
+        "Member Details Requested": confirmMemberID(MemberID)
+        "Message": "Please confirm the request"
+    }), 200
+# dry_run_edit_member

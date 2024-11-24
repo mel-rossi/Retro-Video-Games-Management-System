390e6450440b0059823c5f84b0473c37a1526ddd
mel-rossi <melani.r.rodrigues@gmail.com>
2024-11-23 17:51:55 -0800

    Primary validation : add_game implementation completed

--- AddGame.py
+++ AddGame.py
@@ -2,6 +2,7 @@ import pandas as pd
 from flask_cors import CORS
 from validateEntries import checkTitleEx
 from fetchDetails import get_g, write_games
+from validateEntries import generateLastRank
 from validateEntries import validateYearFormat
 from validateEntries import generateVideoGameID
 from validateEntries import validateGenreFormat
@@ -74,6 +75,58 @@ def dry_run_add_game(Title, Publisher, Year, Inventory, Genre):
     }), 200
 # dry_run_add_game
 
+def fullValidation(Title, Publisher, Year, Inventory, Genre): 
+    if not checkTitleEx(Title) and \
+       validatePublisherFormat(Publisher) and \
+       validateYearFormat(Year) and \
+       validateInventoryFormat(Inventory) and \
+       validateGenreFormat(Genre):
+           return True 
+
+    return False
+# fullValidation
+
+# Primary Validation : Process Input and perform modification if appropriate 
+def add_game(Title, Publisher, Year, Inventory, Genre):
+
+    global df 
+
+    # Primary Validation 
+    if not fullValidation(Title, Publisher, Year, Inventory, Genre): 
+        return jsonify({"error": "Session Transaction Glitch Detected"})
+
+    # Generate the valid next VideoGameID for the Game being added
+    VideoGameID = generateVideoGameID()
+
+    # Set Availability to Available 
+    Availability = 'Available'
+
+    # Generate New Last Rank 
+    Rank = generateLastRank()
+
+    # Make a new row (registration) 
+    row = { 'VideoGameID': VideoGameID, 
+            'Title': Title, 
+            'Publisher': Publisher, 
+            'Year': Year, 
+            'Availability': Availability, 
+            'Inventory': Inventory, 
+            'Genre': Genre, 
+            'Rank': Rank
+    }
+
+    row = pd.DataFrame(row, index=[0]) # Convert to DataFrame 
+
+    df = pd.concat([df, row], ignore_index=True) # Update DataFrame
+
+    # Save updated DataFrame back to CSV file 
+    write_games(df)
+
+    # Return the updated row as JSON
+    return jsonify({"Registration Added": row.to_dict(orient='records')}), 200
+
+# add_game
+
 @addgame_bp.route('/add_game', methods=['POST'])
 def add_game_route(): 
 
@@ -92,6 +145,15 @@ def add_game_route():
                                 data.get('Genre'))
 
     # Confirm: Primary validation and (if valid) add game
+    if data.get('Confirm').lower() == 'confirmed': 
+        Title = session.get('Title')
+        Publisher = session.get('Publisher')
+        Year = session.get('Year')
+        Inventory = session.get('Inventory')
+        Genre = session.get('Genre')
+        return add_game(Title, Publisher, Year, Inventory, Genre)
 
+    else: 
+        return jsonify({"message": "Operation cancelled"}), 200
 # add_game_route
 

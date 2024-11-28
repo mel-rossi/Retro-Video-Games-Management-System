a0c511a55dcee1ad7bad666fa557a110a1f07b57
mel-rossi <melani.r.rodrigues@gmail.com>
2024-11-27 20:16:55 -0800

    Dry Run Edit Game Implementation complete

--- EditGame.py
+++ EditGame.py
@@ -1,9 +1,18 @@
 import pandas as pd 
 from flask_cors import CORS
+from fetchDetails import gameYear
+from fetchDetails import gameGenre
+from fetchDetails import gameTitle
+from fetchDetails import gamePublisher
+from fetchDetails import gameInventory
+from validateEntries import checkTitleEx
 from fetchDetails import get_g, write_games
+from validateEntries import validateNumInsert
+from validateEntries import validateInsertion
 from validateEntries import confirmVideoGameID
 from validateEntries import validateVideoGameID
 from validateEntries import validateGenreFormat
+from validateEntries import validateInventoryFormat
 from flask import request, jsonify, Blueprint, session
 
 # This file allows the user to edit Video Game Registrations 
@@ -32,6 +41,150 @@ def dry_run_request_game(VideoGameID):
     }), 200
 # dry_run_request_game
 
+def changeRequest(Title, Publisher, Year, Inventory, Genre): 
+    if Title is None and \
+       Publisher is None and \
+       Year is None and \
+       Inventory is None and \
+       Genre is None: 
+           return False 
+
+    return True 
+# changeRequest
+
+# Dry Run : Initial Input Validation After Successful Request 
+def dry_run_edit_game(VideoGameID, Title, Publisher, Year, Inventory, Genre, mode):
+
+    no = '[No Changes]'
+
+    # Request Validation 
+    if not validateVideoGameID(VideoGameID): 
+        return jsonify({"error": "Session Transaction Glitch Detected"}), 400
+
+    # Change Validation
+    if not changeRequest(Title, Publisher, Year, Inventory, Genre): 
+        return jsonify({"error": "No Changes are Being Requested"}), 400
+
+
+    # Insert Editing Mode
+    if mode is not None and mode.lower() == 'insert':
+
+        # Insert to Title
+        if Title is not None: # Concat Title 
+            Title = gameTitle(VideoGameID) + Title 
+
+        # Insert to Publisher
+        if Publisher is not None: # Concat Publisher
+            Publisher = gamePublisher(VideoGameID) + Publisher 
+
+        # Insert to Year
+        if Year is not None and validateInsertion(Year) and \
+           validateNumInsert(Year[1:]): 
+               if Year.startswith('+'): # Increment(+) Year #
+                   Year = int(gameYear(VideoGameID)) + \
+                          int(Year[1:])
+
+               else: # Decrement(-) Year #
+                   Year = int(gameYear(VideoGameID)) - \
+                          int(Year[1:])
+
+               Year = str(Year)
+
+        # Insert to Inventory
+        if Inventory is not None and validateInsertion(Inventory) and \
+           validateInventoryFormat(Inventory[1:]):
+               if Inventory.startswith('+'): # Increment(+) Inventory #
+                   Inventory = int(gameInventory(VideoGameID)) + \
+                               int(Inventory[1:])
+
+               else: # Decrement(-) Inventory #
+                   if Inventory[1:] >= gameInventory(VideoGameID): 
+                       Inventory = 0
+
+                   else: 
+                       Inventory = int(gameInventory(VideoGameID)) - \
+                                   int(Inventory[1:])
+
+               Inventory = str(Inventory)
+
+        # Insert to Genre
+        if Genre is not None and validateInsertion(Genre) and \
+           validateGenreFormat(Genre[1:]):
+               if Genre.startswith('+'): # Add Genre(s)
+                   Genre = gameGenre(VideoGameID) + "/" + Genre[1:]
+
+               else: # Delete Genre(s)
+                   if ('/' + Genre[1:]) in gameGenre(VideoGameID): 
+                       Genre = gameGenre(VideoGameID).\
+                               replace(('/' + Genre[1:]), "")
+
+                   elif (Genre[1:] + '/') in gameGenre(VideoGameID): 
+                       Genre = gameGenre(VideoGameID).\
+                               replace((Genre[1:] + '/'), "")
+
+                   else: 
+                       return jsonify({"error": "Genre Requested To Be \
+                               Removed Doesn't Exist or is the only Genre \
+                               this Video Game Is Filed Under"}), 400
+
+    # Change Request Validation 
+
+    # Validate Title 
+    if Title is not None:
+        if checkTitleEx(Title): 
+            return jsonify({"error": "Invalid Requested Changes for Title: \
+                            Title Already Registered in System"}), 400
+
+    # Validate Publisher
+    if Publisher is not None:
+        if not validatePublisherFormat(Publisher): 
+            return jsonify({"error": "Invalid Requested Changes \
+                    for Publisher"}), 400
+
+    # Validate Year
+    if Year is not None: 
+        if not validateYearFormat(Year): 
+            return jsonify({"error": "Invalid Requested Changes for \
+                    Year"}), 400
+
+    # Validate Inventory
+    if Inventory is not None:
+        if not validateInventoryFormat(Inventory):
+            return jsonify({"error": "Invalid Requested Changes for \
+                    Inventory"}), 400
+
+    # Validate Genre 
+    if Genre is not None:
+        if not validateGenreFormat(Genre): 
+            return jsonify({"error": "Invalid Requested Changes for \
+                    Genre"}), 400
+
+    session['VideoGameID'] = VideoGameID
+    session['Title'] = Title 
+    session['Publisher'] = Publisher 
+    session['Year'] = Year
+    session['Inventory'] = Inventory 
+    session['Genre'] = Genre 
+
+    return jsonify({ 
+        "Video Game Registration Being Edited": \
+                confirmVideoGameID(VideoGameID).to_dict(orient='records'),
+        "Editing Mode": "Insert (Increment / Decrement / Concatenate)"
+                if mode is not None and mode.lower == 'insert' \
+                else "Overtype (Overwriting)",
+        "Title Requested Changes": Title \
+                if Title is not None else no, 
+        "Publisher Requested Changes": Publisher \
+                if Publisher is not None else no,
+        "Year Requested Changes": Year \
+                if Year is not None else no, 
+        "Inventory Requested Changes": Inventory \
+                if Inventory is not None else no, 
+        "Genre Requested Changes": Genre \
+                if Genre is not None else no
+    }), 200
+# dry_run_edit_game
+
 @editgame_bp.route('/edit_game', methods=['POST'])
 def edit_game_route(): 
 
@@ -44,5 +197,19 @@ def edit_game_route():
     # Dry Run Request : Initial Validation for Video Game Request and confirmation
     if 'Request' not in data and 'Confirm' not in data: 
         return dry_run_request_game(data.get('VideoGameID'))
+
+    # Dry Run : Initial Validation and confirmation
+    if 'Confirm' not in data: 
+        if data.get('Request').lower() == 'verified': 
+            VideoGameID = session.get('VideoGameID')
+            return dry_run_edit_game(VideoGameID, 
+                                     data.get('Title'), 
+                                     data.get('Publisher'), 
+                                     data.get('Year'), 
+                                     data.get('Inventory'), 
+                                     data.get('Genre'),
+                                     data.get('mode'))
+        else: 
+            return jsonify({"message": "Request cancelled"}), 200
     
 # edit_game_route
